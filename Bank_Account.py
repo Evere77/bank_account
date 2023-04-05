7b6db642a39863ad25b4f52d10a6533f17511ba7
Evere77 <evere777@yahoo.com>
2023-04-05 07:57:48 -0700

    update

--- Bank_Account.py
+++ Bank_Account.py
@@ -1,14 +1,16 @@
 class BankAccount:
     # don't forget to add some default values for these parameters!
-    def __init__(self, int_rate = 0.01, balance = 0):
+    def __init__(self, int_rate, balance):
         # your code here! (remember, instance attributes go here)
         self.int_rate = int_rate
         self.balance = balance
         # don't worry about user info here; we'll involve the User class soon
+
     def deposit(self, amount):
         self.balance += amount
         return self
         # your code here
+
     def withdraw(self, amount):
         if self.balance >= amount:
             self.balance -= amount
@@ -17,10 +19,12 @@ class BankAccount:
             self.balance -= 5
         return self
         # your code here
+
     def display_account_info(self):
         print(f"Balance: ${self.balance}")
         return self
         # your code here
+
     def yield_interest(self):
         if self.balance > 0:
             self.balance += self.balance * self.int_rate
@@ -28,8 +32,10 @@ class BankAccount:
         # your code here
 
 
-account1 = BankAccount()
-account2 = BankAccount()
+account1 = BankAccount(1, 100)
+account2 = BankAccount(2, 50)
 
-account1.deposit(10).deposit(25).deposit(5).withdraw(30).yield_interest().display_account_info()
-account2.deposit(100).deposit(75).withdraw(25).withdraw(25).withdraw(25).withdraw(25).yield_interest().display_account_info()
+account1.deposit(10).deposit(25).deposit(5).withdraw(
+    30).yield_interest().display_account_info()
+account2.deposit(100).deposit(75).withdraw(25).withdraw(25).withdraw(
+    25).withdraw(25).yield_interest().display_account_info()
